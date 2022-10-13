71ee85fcb3a732e7a860fcf81c1580839d826b4f
Renaudin Bryan <bryan.renaudin@epitech.eu>
2022-10-13 15:19:56 +0200

    Grille

---
+++ colle.php
@@ -0,0 +1,499 @@
+<?php
+
+function colle($x, $y,$nombredeXAppeler) 
+{
+
+    if($nombredeXAppeler==1)
+    {
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+        echo $espace ="\n";
+
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+
+    }
+    if($nombredeXAppeler==2)
+    {
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+        echo $espace ="\n";
+
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+        echo $espace ="\n";
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+    }
+    if($nombredeXAppeler==3)
+    {
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+        echo $espace ="\n";
+
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+        echo $espace ="\n";
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+        echo $espace ="\n";
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+    }
+    if($nombredeXAppeler==4)
+    {
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+        echo $espace ="\n";
+
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+        echo $espace ="\n";
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+        echo $espace ="\n";
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+        echo $espace ="\n";
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+    }
+    if($nombredeXAppeler==5)
+    {
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+        echo $espace ="\n";
+
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+        echo $espace ="\n";
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+        echo $espace ="\n";
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+        echo $espace ="\n";
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";} 
+        echo $espace ="\n";
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";} 
+    }
+    if($nombredeXAppeler==6)
+    {
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+        echo $espace ="\n";
+
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+        echo $espace ="\n";
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+        echo $espace ="\n";
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+        echo $espace ="\n";
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";} 
+        echo $espace ="\n";
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";} 
+        echo $espace ="\n";
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";} 
+    }
+    if($nombredeXAppeler==7)
+    {
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+        echo $espace ="\n";
+
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+        echo $espace ="\n";
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+        echo $espace ="\n";
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";}
+        echo $espace ="\n";
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";} 
+        echo $espace ="\n";
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";} 
+        echo $espace ="\n";
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";} 
+        echo $espace ="\n";
+        for($i=0; $i<$x; $i++)
+        {
+           echo  $barre = "|   ";
+        
+
+        }
+        echo $barre .= "\n";
+
+        for($i=0; $i<$y-1; $i++)
+        {
+            echo $croix = "+---";
+
+        }
+        if($i==$y-1){echo $lastPlus ="+";} 
+    }
+}
+
+    colle(3,4,7);
